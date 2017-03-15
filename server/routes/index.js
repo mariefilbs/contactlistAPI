46002f39bb37e92da9e8d9655c9aa7e7e2eb8f17
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-15 08:45:36 -0400

    Setting up heroku.

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@
 
 const ContactsController = require("../controllers/contacts");
 
+
 module.exports = (app) => {
   app.post('/contacts', ContactsController.create);
   app.get('/contacts', ContactsController.listContacts);
