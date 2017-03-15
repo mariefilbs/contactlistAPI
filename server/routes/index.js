ee4e261d1a648842127bb99978bb9fe9daf4ac78
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-15 16:27:44 -0400

    Updated App file and routes.

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,13 @@
 
 const ContactsController = require("../controllers/contacts");
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
- });
- 
 module.exports = (app) => {
+  app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+   });
+
   app.post('/contacts', ContactsController.create);
   app.get('/contacts', ContactsController.listContacts);
   app.get('/contacts/:id', ContactsController.listOneContact);
