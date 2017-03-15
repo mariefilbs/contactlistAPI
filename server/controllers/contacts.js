6626d01281a4ff35552a6dc75f39509437112924
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-15 17:26:08 -0400

    Added a static profile photo.

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -1,4 +1,5 @@
 const Contacts = require("../models").Contacts;
+const staticPhoto = 'https://www.google.com/imgres?imgurl=http%3A%2F%2Fsunfieldfarm.org%2Fwp-content%2Fuploads%2F2014%2F02%2Fprofile-placeholder.png&imgrefurl=http%3A%2F%2Fsunfieldfarm.org%2Fabout%2Ffaculty%2Fprofile-placeholder%2F&docid=NUs1HfRIwhI3DM&tbnid=wqFTpxdyrrUpwM%3A&vet=1&w=600&h=600&bih=659&biw=1280&q=photo%20placeholder&ved=0ahUKEwj9xrfquNnSAhWBJCYKHSj3C94QMwg3KAAwAA&iact=mrc&uact=8';
 
 module.exports = {
   create (req, res) {
@@ -7,7 +8,7 @@ module.exports = {
       phone_number: req.body.phone_number,
       city: req.body.city,
       state: req.body.state,
-      photo_URL: req.body.photo_URL,
+      photo_URL: req.body.photo_URL || 'staticPhoto',
       website: req.body.website
     })
       .then(contacts => res.status(201).send(contacts))
